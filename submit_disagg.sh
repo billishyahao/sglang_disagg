5e43a275bab3d2a33e3eb0a02d076bc0d9893f76
billishyahao <bill.he@amd.com>
2026-01-16 11:06:31 +0000

    exclude the nodelist

--- submit_disagg.sh
+++ submit_disagg.sh
@@ -100,7 +100,7 @@ sbatch_cmd=(
     --time "$TIME_LIMIT" 
     --partition "$SLURM_PARTITION" 
     --account "$SLURM_ACCOUNT"
-    --nodelist smci355-ccs-aus-n08-[21,25,29]
+    --exclude smci355-ccs-aus-n08-[21,25,29]
     --job-name ${xP}p${yD}d_bench-serving
     run_xPyD_models.slurm
 )

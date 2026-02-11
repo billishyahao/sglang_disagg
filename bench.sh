c83719b278b0bb8e1048993b77ac72d1565663a8
billishyahao <bill.he@amd.com>
2026-02-11 04:39:48 +0000

    revert comment out chat template

--- bench.sh
+++ bench.sh
@@ -53,8 +53,8 @@ for max_concurrency in ${chosen_concurrencies[@]}; do
         --num-prompts $(( $max_concurrency * $num_prompts_multiplier )) \
         --max-concurrency "$max_concurrency" \
         --result-filename "$export_file" \
-        --result-dir /workspace/
-        # $( [ "$IS_MTP" = "true" ] && echo "--use-chat-template" )
+        --result-dir /workspace/ \
+        $( [ "$IS_MTP" = "true" ] && echo "--use-chat-template" )
 
     echo "-----------------------------------------"
     sleep 5
